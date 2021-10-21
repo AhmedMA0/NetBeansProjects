d6331715647e91197d07f171fa82f863f61a9d52
AhmedMA0 <10253@iespuertasdelcampo.es>
2021-10-21 13:00:43 +0200

    6

---
+++ diseño/1/result.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Document html">
+    <meta name="author" content="Ahmed R.">
+    <meta name="keywords" content="computers,programing,web design,html,html5">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="lang" content="es-ES">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = $_POST['name'];
+
+        echo $a;
+    ?>
+</body>
+</html>
